3ef334ee0883274643aadea1e77f981f22aacf03
Fistbutter <djlehnen@gmail.com>
2015-10-29 18:08:01 -0400

    Angular routing works properly, added navigation buttons

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,17 @@
-angular.modules("ngDreams", ["ngRoute"]);
+angular.module("ngDreams", ["ngRoute"]);
 
 angular.module("ngRoute")
         .config(function($routeProvider){
             $routeProvider
             .when("/dreamform",{
-              templateUrl:"html/dreamform.html"
+              templateUrl:"html/dreamform.html",
               controller:"DreamController"
             })
-            .when("/Recall"{
-              templateUrl:"html/recall.html"
+            .when("/recall",{
+              templateUrl:"html/recall.html",
               controller:"RecallController"
             })
-            .otherwise(
+            .otherwise({
                 redirectTo:"/dreamform"
-            )
+            });
         });
