ba8edf048d3663499d76a037cf649c00d64e01f1
Fistbutter <djlehnen@gmail.com>
2015-11-02 20:49:38 -0500

    fixed some minor visual problems and cleaned up code remains

--- public/js/controllers/recallController.js
+++ public/js/controllers/recallController.js
@@ -4,23 +4,7 @@ angular.module("ngDreams")
             $http.get("/api/dreams")
                 .then(function(response){
                   $scope.dreams = response.data;
-                  // var totalArray = response.data;
-                  // $scope.totalItems = totalArray.length;
-                  // console.log($scope.totalItems);
                   console.log($scope.dreams);
-
-
-                  // $scope.itemsPerPage = 10;
-                  // $scope.currentPage = 1;
-                  // $scope.pageCount = function () {
-                  //     return Math.ceil($scope.totalItems / $scope.itemsPerPage);
-                  //   };
-                  // $scope.totalItems.$promise.then(function(){
-                  //     $scope.watch('currentPage + itemsPerPage', function(){
-                  //       var begin = (($scope.currentPage - 1) * $scope.itemsPerPage),
-                  //         end = begin + $scope.itemsPerPage;
-                  //     });
-                  // });
                 });
             $scope.removeDream = function(dream){
               $http.delete("/api/dreams/" + dream._id)
@@ -33,16 +17,3 @@ angular.module("ngDreams")
             };
 
         });
-
-        // app.controller('contentCtrl', function ($scope, friendsFactory) {
-        //
-        //
-        //   $scope.friends.$promise.then(function () {
-        //     $scope.$watch('currentPage + itemsPerPage', function() {
-        //       var begin = (($scope.currentPage - 1) * $scope.itemsPerPage),
-        //         end = begin + $scope.itemsPerPage;
-        //
-        //       $scope.filteredFriends = $scope.friends.slice(begin, end);
-        //     });
-        //   });
-        // });
